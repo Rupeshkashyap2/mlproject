5cb04d26e9c6dc6c1094b06b967eb8cca7e70808
Rupeshkashyap2 <rupeshkashyaprupeshkashyap412@gmail.com>
2026-07-02 23:56:17 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+HYPEN_E_DOT='-e .'
+def get_requirments(file_path:str)->List[str]:
+  ''' this function will return the list of requirments
+  '''
+  requirments=[]
+  with open(file_path) as file_obj:
+    requirments=file_obj.readlines()
+    requirments = [req.replace("\n","") for req in requirments] 
+
+    if HYPEN_E_DOT in requirments:
+      requirments.remove(HYPEN_E_DOT)
+
+    return requirments
+ 
+
+setup(
+    name="mlproject",
+    version="0.1.0",
+    author="Rupesh kumar kashyap",
+    author_email="rupeshkashyaprupeshkashyap412@gmail.com",
+    install_requires=get_requirments('requirements.txt'),
+    packages=find_packages(),
+
+
+)
